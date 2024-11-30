73dc717eeb6763bd41d40b0d4a2e93b49445c1ea
Ben <ben@MEFAs-iMac.local>
2024-11-30 01:09:44 -0500

    change

--- terraform-remote-backend1/backend.tf
+++ terraform-remote-backend1/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w6bcbucket"
+    bucket = ""
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock2"
+    dynamodb_table = ""
     encrypt = true
   }
 }
